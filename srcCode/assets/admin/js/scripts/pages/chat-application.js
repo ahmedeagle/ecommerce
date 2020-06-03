18f5295635174de85e63ceec3be7ffcceac9b4be
= <=>
2020-06-03 21:00:58 +0300

    =

--- srcCode/assets/admin/js/scripts/pages/chat-application.js
+++
@@ -1,6 +0,0 @@
-//  Notifications & messages scrollable
-if($('.sidebar-fixed').length > 0){
-    $('.sidebar-fixed').perfectScrollbar({
-        theme:"dark"
-    });
-}
--- srcCode/assets/admin/js/scripts/pages/email-application.js
+++
@@ -1,6 +0,0 @@
-//  Notifications & messages scrollable
-if($('#users-list').length > 0){
-    $('#users-list').perfectScrollbar({
-        theme:"dark"
-    });
-}
